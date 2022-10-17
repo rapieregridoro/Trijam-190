4d9b7037b3ed5a3d0f615bafdb97cf129b50849b
Rapiere Gridoro <rapieregridoro.gmail.com>
2022-10-17 01:08:26 -0300

    Inimigos respawnam

--- Trijam 190/Scripts/Fantasma.gd
+++ Trijam 190/Scripts/Fantasma.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 
 var fugir_de := Vector2.ZERO
+var Tempo:float = 10
 
 func _ready() -> void:
 	
@@ -11,11 +12,18 @@ func _physics_process(delta: float) -> void:
 	fugir_de = Global.Player.global_position - position
 	
 	move_and_slide(-fugir_de.normalized()* 25)
+	Tempo -= delta
+	if Tempo < 0:
+		var Ini = load("res://Scenes/Inimigo.tscn")
+		var enemego = Ini.instance()
+		enemego.position = position
+		get_parent().add_child(enemego)
+		queue_free()
 	
-
 
 func _on_Hurt_body_entered(body: Node) -> void:
 	Global.Inimigos += 1
 	queue_free()
+	
 
 
